044308fc2ab6815507c423344f0c52441a21e2ff
DRAGON <93909131+taher-1@users.noreply.github.com>
2022-05-31 22:36:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 BOT_TOKEN = "5034999677:AAFKKUsI4gB8XJPiX49MYJrkv3BC6gjWDgc"
-APP_URL = "https://bot-check-users-telegram-2022.herokuapp.com/" + BOT_TOKEN
+APP_URL = "https://bot-90.herokuapp.com/" + BOT_TOKEN
